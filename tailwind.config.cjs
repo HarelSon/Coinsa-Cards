b0857c5f570cdfda612408e76c57c0974dbf46c1
Joseph Ramos <jardix22@gmail.com>
2023-02-24 15:20:59 -0500

    Add banner component astro.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,20 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: ["Open Sans", "sans-serif"]
-			},
-			colors:{
-				as:{
-					blue: "rgb(0, 95, 226)",
-					"blue-dark": "#273475",
-					green: "#16993b",
-					black: "rgb(51, 51, 51)",
-				}
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        screens: {
+          sm: "640px",
+          md: "768px",
+          lg: "1024px",
+          // xl: "1280px",
+          // '2xl': '1536px',
+        },
+      },
+      fontFamily: {
+        sans: ["Open Sans", "sans-serif"],
+        display: ["Oswald"],
+      },
+      colors: {
+        as: {
+          blue: "rgb(0, 95, 226)",
+          "blue-dark": "#273475",
+          green: "#16993b",
+          black: "rgb(51, 51, 51)",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
